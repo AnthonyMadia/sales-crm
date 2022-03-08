27c9a9e5fea3ba6d67a706bc472c81f6589ab149
Anthony Madia <aamadia24@gmail.com>
2022-03-08 13:07:00 -0500

    Change auth router

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,7 @@ router.get(
 )
 
 router.get(
-  '/google/oauth2callback',
+  'sales-crm-men-app.herokuapp.com/auth/google/oauth2callback',
   passport.authenticate('google', {
     successRedirect: '/leads',
     failureRedirect: '/auth/google',
